19a2d64757d39ef111596d04f5ebca5a6faf837a
Brayden <Braydenlwong@gmail.com>
2023-07-06 21:54:20 -0600

    app uses environment variables

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const config = app.get<ConfigService>(ConfigService);
+  const PORT = config.get('PORT');
+
+  await app.listen(PORT, async () =>
+    console.log(`Server started at ${await app.getUrl()}`),
+  );
 }
 bootstrap();
